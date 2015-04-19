3fbbe97f11cde1487fd2cc10fd318a4070d3bb1b
Dan Clawson <clawson@uen.org>
2015-04-19 16:24:26 -0600

    Add color scheme files.

---
+++ ProjectSpy/UIColor+PSColor.h
@@ -0,0 +1,37 @@
+//
+//  PSColor.h
+//  ProjectSpy
+//
+//  Created by Dan Clawson on 4/19/15.
+//  Copyright (c) 2015 Slacker Tools. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (PSColor)
+
++ (UIColor *) psPrimary0;
++ (UIColor *) psPrimary1;
++ (UIColor *) psPrimary2;
++ (UIColor *) psPrimary3;
++ (UIColor *) psPrimary4;
+
++ (UIColor *) psSecondaryOne0;
++ (UIColor *) psSecondaryOne1;
++ (UIColor *) psSecondaryOne2;
++ (UIColor *) psSecondaryOne3;
++ (UIColor *) psSecondaryOne4;
+
++ (UIColor *) psSecondaryTwo0;
++ (UIColor *) psSecondaryTwo1;
++ (UIColor *) psSecondaryTwo2;
++ (UIColor *) psSecondaryTwo3;
++ (UIColor *) psSecondaryTwo4;
+
++ (UIColor *) psComplement0;
++ (UIColor *) psComplement1;
++ (UIColor *) psComplement2;
++ (UIColor *) psComplement3;
++ (UIColor *) psComplement4;
+
+@end
